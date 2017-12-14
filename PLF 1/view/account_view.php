5632d27ce9f6fead2d5ad874c72f92d49a10332a
hehnm <hehnm@edu.htl-villach.atgit config --global user.email hehnm@edu.htl-villach.atgit config --global user.email hehnm@edu.htl-villach.at>
2017-12-14 11:23:56 +0100

    11:00:26-DESKTOP-PBAA86J-Wörk

--- PLF 1/view/account_view.php
+++ PLF 1/view/account_view.php
@@ -3,14 +3,17 @@
 <body>
 <form method="post">
     <input type="text" name="owner" placeholder="your Name">
+    <br>
     <label>Describe your account</label><input type="text" name="description" placeholder="optional">
+    <br>
     <select>
         <option name="acc" value="g">Girokonto</option>
         <option name="acc" value="k">Kreditkonto</option>
         <option name="acc" value="s">Sparkonto</option>
         <option name="acc" value="d"> Depot</option>
     </select>
-    <input type="number">
+    <input type="text" placeholder="Eröffnungsjahr" name="year">
+    <input type="text" placeholder="Laufzeit" name="runtime">
     <input type="button" name="save" value="Save">
 </form>
 </body>
