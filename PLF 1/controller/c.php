e7c402a333285e54620259c76069cac74a8a6f5e
hehnm <hehnm@edu.htl-villach.atgit config --global user.email hehnm@edu.htl-villach.atgit config --global user.email hehnm@edu.htl-villach.at>
2017-12-14 12:08:41 +0100

    11:00:26-DESKTOP-PBAA86J-Wörk

--- PLF 1/controller/c.php
+++ PLF 1/controller/c.php
@@ -1,16 +1,18 @@
 <?php
-$login=true;
-if($login==true)
-    require_once 'view/account_view.php';
-
+$message='';
 if(!isset($_SESSION['account']))
 {
     $_SESSION['account'] = new account();
+}
+else
+{
+ $message='There is already a account';
+ return $message;
 }
 $Account =&$_SESSION['account'];
 
 if(!isset($_POST['save']))
 {
     if($Account->setYear($_POST['year']))
-        return;
+        
 }
