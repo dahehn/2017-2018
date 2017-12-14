b87d23791ce380bad0550baaa86f43bd7c17037e
hehnm <hehnm@edu.htl-villach.atgit config --global user.email hehnm@edu.htl-villach.atgit config --global user.email hehnm@edu.htl-villach.at>
2017-12-14 12:14:46 +0100

    11:00:26-DESKTOP-PBAA86J-Wörk

--- PLF 1/controller/c.php
+++ PLF 1/controller/c.php
@@ -1,21 +1,28 @@
 <?php
 $message='';
-require_once 'view/account_view.php';
-if(!isset($_SESSION['account']))
+$fin=false;
+if($fin=false)
 {
-    $_SESSION['account'] = new account();
-}
-else
-{
- $message='There is already a account';
- return $message;
-}
-$Account =&$_SESSION['account'];
-
-if(!isset($_POST['save']))
-{
-    if($Account->setYear($_POST['year']))
+    require_once 'view/account_view.php';
+    if(!isset($_SESSION['account']))
     {
+        $_SESSION['account'] = new account();
+    }
+    else
+    {
+        $message='There is already a account';
+        return $message;
+    }
+    $Account =&$_SESSION['account'];
 
+    if(!isset($_POST['save']))
+    {
+        if($Account->setYear($_POST['year']) && $Account->setRuntime($_POST['runtime']) && $Account->setOwner($_Post['owner']) )
+        {
+            $fin = true;
+            return $fin;
+        }
     }
 }
+else
+    require_once 'view/transaction_view.php';
