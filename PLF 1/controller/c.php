3d19989e94940c9647051e9ac5f8b3eea01872b5
hehnm <hehnm@edu.htl-villach.atgit config --global user.email hehnm@edu.htl-villach.atgit config --global user.email hehnm@edu.htl-villach.at>
2017-12-14 11:54:27 +0100

    11:00:26-DESKTOP-PBAA86J-Wörk

--- PLF 1/controller/c.php
+++ PLF 1/controller/c.php
@@ -5,13 +5,9 @@ if(!isset($_SESSION['account']))
     $_SESSION['account']= new account();
     require_once 'view/account_view.php';
 }
-$Account=$_SESSION['account'];
+$Account = $_SESSION['account'];
 if(!isset($_POST['save']))
 {
-    if ($Account->setOwner($_POST['owner']) && $Account->setRuntime($_POST['runtime']) && $Account->setYear($_POST['year'])){
-
-    }
-
-
-
+    if($Account->setOwner($_POST['owner']))
+        require_once 'view/transaction_view.php';
 }
