de8d7860174ec42b6af0bb77fe863d4e9602d4a1
hehnm <hehnm@edu.htl-villach.atgit config --global user.email hehnm@edu.htl-villach.atgit config --global user.email hehnm@edu.htl-villach.at>
2018-01-11 11:43:02 +0100

    10:46:21-DESKTOP-PBAA86J-Wörk

--- SEWPLF2_hehnm/views/edit_view.php
+++ SEWPLF2_hehnm/views/edit_view.php
@@ -1,16 +1,19 @@
+
 <html>
 <body>
 <h1>Product:</h1>
 <?php echo $message?>
 <form method="post">
-    Name<input type="text" name="newName" value="'.pr.'">
+    <?php
+    'Name<input type="text" name="newName" value="'.product->getName().'">
     <br>
     Price<input type="text" name="newPrice">
     <br>
     Amount<input type="text" name="newAmount">
     <br>
     <input type="submit" name="save" value="Save">
-    <input type="submit" name="cancel" value="Cancel">
+    <input type="submit" name="cancel" value="Cancel">'
+    ?>
 </form>
 
 </body>
