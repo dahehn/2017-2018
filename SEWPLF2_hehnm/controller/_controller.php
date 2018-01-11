5c33c948c37adb164f436f6e43770280ffb8f09a
hehnm <hehnm@edu.htl-villach.atgit config --global user.email hehnm@edu.htl-villach.atgit config --global user.email hehnm@edu.htl-villach.at>
2018-01-11 11:06:39 +0100

    10:46:21-DESKTOP-PBAA86J-Wörk

--- SEWPLF2_hehnm/controller/_controller.php
+++ SEWPLF2_hehnm/controller/_controller.php
@@ -7,7 +7,7 @@ if(!isset($_SESSION['products']))
 $products =$_SESSION['products'];
 if(isset($_POST['new']))
 {
-    require_once 'views/new_view.php';
+    require_once 'views/new_view..php';
     return;
 }
 
