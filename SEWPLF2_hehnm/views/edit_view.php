a38231fe2e7e76d5045c1d51fa9acf8c498a97ed
hehnm <hehnm@edu.htl-villach.atgit config --global user.email hehnm@edu.htl-villach.atgit config --global user.email hehnm@edu.htl-villach.at>
2018-01-11 12:15:20 +0100

    10:46:21-DESKTOP-PBAA86J-Wörk

--- SEWPLF2_hehnm/views/edit_view.php
+++ SEWPLF2_hehnm/views/edit_view.php
@@ -9,13 +9,14 @@ function print_textBox($name,$value)
 <h1>Product:</h1>
 
 <form method="post">
-    <?php echo '<input type="text" value="'.$product->getName().'" name="id" disabled '?>
+    Name<?php echo '<input type="text" value="'.$product->getName().'" name="id" disabled '?>
     <br>
     <br>
-    <?php echo'<input type="text" value="'.$product->getPrice().'" name="chnaged"'?>
+    Price<?php echo'<input type="text" value="'.$product->getPrice().'" name="chnaged"'?>
+    <br>
     <input type="submit" name="save" value="Save">
     <input type="submit" name="cancel" value="Cancel">'
-    ?>
+
 </form>
 
 </body>
